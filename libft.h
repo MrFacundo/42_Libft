9cdc8ca0079bca521f5aa6153cb9ec76753e947e
Facundo Troitero <ftroiter@c2r11s4.42lisboa.com>
2022-10-26 20:34:20 +0100

    fixed libt.h

--- libft.h
+++ libft.h
@@ -6,22 +6,20 @@
 /*   By: ftroiter <ftroiter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 18:26:05 by ftroiter          #+#    #+#             */
-/*   Updated: 2022/10/26 20:27:37 by ftroiter         ###   ########.fr       */
+/*   Updated: 2022/10/26 20:33:29 by ftroiter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-void			ft_isalnum(char *str, int n);
+void			ft_bzero(char *str, int n);
 int				ft_isalnum(int c);
 int				ft_isalpha(int c);
 int				ft_isascii(int c);
 int				ft_isdigit(int c);
 int				ft_isprint(int c);
-char			*ft_isprint(char *s, int c, int n);
+char			*ft_memset(char *s, int c, int n);
 int				ft_strlen(char *s);
-
-
 
 #endif
